c093cca76b38a5fcc78b7ebe2bd124363edaa187
DebajyotiBhatta <bdebajyoti22@gmail.com>
2024-12-27 22:42:43 +0530

    Update find-all-duplicates-in-an-array.java
    adding brute-force

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,3 +1,25 @@
+//brute-force TC=O(n^2) sc=O(1)
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+       List<Integer> li=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int c=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    c++;
+                }
+            }
+           if(c==2 && !li.contains(nums[i])){
+            li.add(nums[i]);
+           }
+        }
+        
+        return li;
+    }
+}
+
+
+//optimal Tc=O(n) sc=O(n)
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         Map<Integer,Integer> map=new HashMap<>();
