309dadb9ba3ac4d4b55b211baf8214f36d1ddb95
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-01-04 16:50:21 +0530

    Time: 178 ms (65.97%) | Memory: 45.7 MB (39.53%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,21 +1,18 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        HashSet<Character> st=new HashSet<>();
+        HashMap<Character,Integer> firstOcc=new HashMap<>();
+         HashMap<Character,Integer> lastOcc=new HashMap<>();
         int n=s.length(),count=0;
             for(int i=0;i<n;i++){
-                st.add(s.charAt(i));
+              char ch=s.charAt(i);
+              if(!firstOcc.containsKey(ch)){
+                firstOcc.put(ch,i);
+              }
+              lastOcc.put(ch,i);
             }
-            for(char ch : st){
-               int first=-1;
-               int sec=-1;
-               for(int i=0;i<n;i++){
-                    if(ch==s.charAt(i)){
-                        if(first==-1){
-                            first=i;
-                        }
-                        sec=i;
-                    }
-                }
+            for(char ch : firstOcc.keySet()){
+               int first=firstOcc.get(ch);
+               int sec=lastOcc.get(ch);
                 if(first == sec)continue;
                   HashSet<Character> st1=new HashSet<>();
             for(int i=first+1;i<sec;i++){
