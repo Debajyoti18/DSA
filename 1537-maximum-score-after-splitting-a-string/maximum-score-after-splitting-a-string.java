49c0b5a55eabf1d23a4b402f9aa893e56b7ce0e4
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-01-04 16:51:57 +0530

    Update maximum-score-after-splitting-a-string.java

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,4 +1,6 @@
-class Solution {
+
+tc-O(N)+O(N)+O(N)*26
+    class Solution {
     public int maxScore(String s) {
         int sum=0,totalones=0,zeros=0;
         for (char c : s.toCharArray()){
@@ -16,3 +18,31 @@ class Solution {
         return sum;
     }
 }
+tc-O(N)+O(N)*26
+    class Solution {
+    public int countPalindromicSubsequence(String s) {
+        HashMap<Character,Integer> firstOcc=new HashMap<>();
+         HashMap<Character,Integer> lastOcc=new HashMap<>();
+        int n=s.length(),count=0;
+            for(int i=0;i<n;i++){
+              char ch=s.charAt(i);
+              if(!firstOcc.containsKey(ch)){
+                firstOcc.put(ch,i);
+              }
+              lastOcc.put(ch,i);
+            }
+            for(char ch : firstOcc.keySet()){
+               int first=firstOcc.get(ch);
+               int sec=lastOcc.get(ch);
+                if(first == sec)continue;
+                  HashSet<Character> st1=new HashSet<>();
+            for(int i=first+1;i<sec;i++){
+                    st1.add(s.charAt(i));
+                  }
+                  count += st1.size();
+            }
+            return count;
+    }
+}
+    
+    
