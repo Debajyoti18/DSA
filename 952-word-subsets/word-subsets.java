89d1a04242dd5eed509af1b5a68fde9fabd0cc92
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-01-10 20:50:41 +0530

    Time: 13 ms (81.40%) | Memory: 53.6 MB (95.93%) - LeetSync

---
+++ 952-word-subsets/word-subsets.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] freq=new int[26];
+        for(String str : words2){
+            int[] temp = getFrequency(str);
+            for(int i=0; i < 26; i++){
+                freq[i] = Math.max(freq[i],temp[i]);
+            }
+        }
+        List<String> res = new ArrayList<>();
+        for(String str : words1){
+            int[] temp = getFrequency(str);
+            boolean flag = true;
+            for(int i=0; i < 26; i++){
+              if(freq[i] > temp[i]){
+                flag = false;
+                break;
+              }
+            }
+            if(flag){
+                res.add(str);
+            }
+        }
+        return res;
+    }
+    public int[] getFrequency(String s){
+         int[] freq=new int[26];
+         for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+         }
+         return freq;
+    }
+
+}
