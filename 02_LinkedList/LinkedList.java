4f1342a6f52de8cc4a751aba82bdd77a647a5dce
Debajyoti18 <bdebajyoti22@.com>
2025-08-15 18:52:49 +0530

    Linked List

--- 02_LinkedList/LinkedList.java
+++ 02_LinkedList/LinkedList.java
@@ -83,26 +83,51 @@ public class LinkedList {
 
     // Get First Element -> O(1)
     public int getFirst() {
+        if(head == null){
+            return -1;
+        }
+        return head.data;
         
     }
 
     // Get Last Element -> O(1) { Provided Tail Pointer }
     public int getLast() {
+        if(tail == null){
+            return -1;
+        }
+        return tail.data;
         
     }
 
     // Get Element at given index -> O(N) Worst Case
     public int getAt(int idx) {
+        if(idx == 0){
+           return getFirst();
+        }else if(idx == size){
+           return getLast();
+        }else{
+            Node t = head;
+            for(int i=0;i<idx;i++){
+                t = t.next;
+            }
+            return t.data;  
+        }
        
     }
 
     // Get Node at given Index -> O(N)
     public Node getNodeAt(int idx) {
-     
+      Node t = head;
+            for(int i=0;i<idx;i++){
+                t = t.next;
+            }
+            return t;  
     }
 
     // Remove Last Element -> O(N) {Singly Linked List}
     public void removeLast() {
+        
+    }
        
 
     // Remove Element at Given Index -> O(N) Worst Case
