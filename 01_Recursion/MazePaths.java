112ec0d7c9442a450553c6297ac77c0b41087ad0
Debajyoti18 <bdebajyoti22@.com>
2025-08-24 23:38:42 +0530

    with digonal jumps

--- 01_Recursion/MazePaths.java
+++ 01_Recursion/MazePaths.java
@@ -13,8 +13,22 @@ public class MazePaths {
             }
             System.out.println();
         }
-    }
 
+
+        ArrayList<String> res = withDigonalJumps(1,1,3,3);
+        if (res.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            for (String p : res) {
+                System.out.print(p + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+    ///////////////////  NO DIGONAL JUMPS //////////////////
+        //Time -O(4 power n*n) : less efficient more stack calls .space O(n*n)
     private static ArrayList<String> ratInMaze(int sc, int sr, int dc, int dr) {
         if(sr == dr && sc == dc){
              ArrayList<String> result = new ArrayList<>();
@@ -35,6 +49,43 @@ public class MazePaths {
              }
         }
         return res;
+    }
+
+    //  DIGONAL JUMPS ALLOWED//
+    private static ArrayList<String> withDigonalJumps(int sc,int sr ,int dc, int dr){
+        if(sr == dr && sc == dc){
+             ArrayList<String> result = new ArrayList<>();
+             result.add("");
+             return result;
+        }
+        
+         ArrayList<String> res = new ArrayList<>();
+         // horrizontal moves
+        for(int i = 1; i <=  dc - sc ; i++){
+             ArrayList<String> hpaths = ratInMaze(sc+i, sr, dc, dr);
+             for(String s : hpaths){
+                res.add("h" + i + s);//hh1 hh2 hh3
+             }
+
+        }
+        // vertical moves
+        for(int i = 1; i <=  dr - sr ; i++){
+             ArrayList<String> vpaths = ratInMaze(sc, sr+i, dc, dr);
+             for(String s : vpaths){
+                res.add("v" + i + s);//vv1 vv2 vv3
+             }
+
+        }
+        //digonal moves
+        for(int i = 1; i <=  dc - sc  && i<= dr - sr; i++){
+             ArrayList<String> dpaths = ratInMaze(sc+i, sr+i, dc, dr);
+             for(String s : dpaths){
+                res.add("d" + i + s);
+             }
+
+        } 
+        return  res;
+
     }
 
     
