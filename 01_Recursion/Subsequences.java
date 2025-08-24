3280596ab85256e1b30db7cd322e604a684d0b5d
Debajyoti18 <bdebajyoti22@.com>
2025-08-24 23:49:47 +0530

    printing Subsequences

--- 01_Recursion/Subsequences.java
+++ 01_Recursion/Subsequences.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 public class Subsequences {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the String :");
         String inp =  sc. nextLine();
         ArrayList<String> res = findSubsequences(inp);
         for (String s: res ){
             System.out.print(s+" ");
         }
+        System.out.println("Printing Subsequences :");
+        printSubsequences(inp, "");
     }
 
     private static ArrayList<String> findSubsequences(String inp) {
@@ -29,5 +32,18 @@ public class Subsequences {
             res.add( ch +c );
         }
         return res;
+    }
+    //printing Subsequences - Less number of Stack calls.TC: 0(logn)
+    private static void printSubsequences(String inp , String op){
+        if(inp.length() == 0){
+            System.out.println(op);
+            return;
+        }
+        char ch = inp.charAt(0);
+        String rest = inp.substring(1);
+       
+        printSubsequences(rest, op + "");
+        printSubsequences(rest, op + ch);
+
     }
 }
