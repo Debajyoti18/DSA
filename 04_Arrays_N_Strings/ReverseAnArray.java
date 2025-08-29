3f123b11edc86436cf2ac683edc31b1ab21e724c
Debajyoti18 <bdebajyoti22@.com>
2025-08-29 23:22:35 +0530

    array reverse

---
+++ 04_Arrays_N_Strings/ReverseAnArray.java
@@ -0,0 +1,23 @@
+public class ReverseAnArray {
+    public static void main(String[] args) {
+        int[]  arr = { 12 , 13, 14, 15 ,17};
+        reverse(arr);
+    }
+
+    private static void reverse(int[] arr) {
+        // TODO Auto-generated method stub
+        int i = 0;
+        int j = arr.length -1;
+        while(j > i){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j--;i++;
+        }
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+       
+    }
+}
--- 04_Arrays_N_Strings/SumOfArrays.java
+++ 04_Arrays_N_Strings/SumOfArrays.java
@@ -35,4 +35,5 @@ public class SumOfArrays {
         }
         System.out.println();
     }
+    
 }
