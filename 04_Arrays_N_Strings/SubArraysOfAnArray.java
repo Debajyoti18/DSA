0cece559b97141855e9f161cc83529b42f3773b7
Debajyoti18 <bdebajyoti22@.com>
2025-08-30 21:21:51 +0530

    subarrays

---
+++ 04_Arrays_N_Strings/SubArraysOfAnArray.java
@@ -0,0 +1,16 @@
+public class SubArraysOfAnArray {
+    public static void main(String[] args) {
+        String[] arr = {"a","b","c","d"};
+        printSubarrays(arr);
+    }
+    public static void printSubarrays(String[] arr){
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = i ; j < arr.length ; j++){
+                for(int k = i ; k <= j; k++){
+                    System.out.print(arr[k] + "  ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
