784dedd737ae991f74cd99c04ae2bee3bc4d9d29
Debajyoti18 <bdebajyoti22@.com>
2025-09-09 15:35:20 +0530

    next smaller to left n right

--- 05_Stack_N_Queues/NextGreaterToLeft.java
+++ 05_Stack_N_Queues/NextGreaterToLeft.java
@@ -3,50 +3,51 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Stack;
 
-public class NextGreaterToRight {
+public class NextGreaterToLeft {
     public static void main(String[] args) {
-        int[] arr = {12,33,65,98,123,5543,755};
-        System.out.println("Next Greater element to right of elements are :");
-       ArrayList<Integer> li = findNextRight(arr);
+        int[] arr = {12,32223,6534,98,123,5543,755};
+        System.out.println("Next Greater element to left of elements are :");
+       ArrayList<Integer> li = findNextLeft(arr);
        System.out.println(li);
-        ArrayList<Integer> list = findNextSmallerToRight(arr);
-        System.out.println("Next Smaller element to right of elements are :");
-        System.out.println(list);
+        ArrayList<Integer> list = findNextSmallerToLeft(arr);
+             System.out.println("Next smaller element to left of elements are :");
+             System.out.println(list);
     }
 
-    private static ArrayList<Integer> findNextRight(int[] arr) {
+    private static ArrayList<Integer> findNextLeft(int[] arr) {
             Stack<Integer> stack = new Stack<>();
             ArrayList<Integer> li = new ArrayList<>();
-            for(int i = arr.length - 1; i >= 0; i--){
+            for(int i = 0 ; i  < arr.length ; i++ ){
                 while(!stack.isEmpty() && stack.peek() <= arr[i]){ //pop until greater is found 
                     stack.pop();
                 }
-                if(stack.isEmpty()){//if  empty then no greater to right
+                if(stack.isEmpty()){//if  empty then no greater to left
                     li.add(-1);
                 }else{
-                    li.add(stack.peek());// top is the required right greater
+                    li.add(stack.peek());// top is the required left greater
                 }
                 stack.push(arr[i]);
             }
-            Collections.reverse(li);
+            
             return li;
             
     }
-    private static ArrayList<Integer> findNextSmallerToRight(int[] arr) {
+
+    private static ArrayList<Integer> findNextSmallerToLeft(int[] arr) {
             Stack<Integer> stack = new Stack<>();
             ArrayList<Integer> li = new ArrayList<>();
-            for(int i = arr.length - 1; i >= 0; i--){
+            for(int i = 0 ; i  < arr.length ; i++ ){
                 while(!stack.isEmpty() && stack.peek() >= arr[i]){ //pop until smaller is found 
                     stack.pop();
                 }
-                if(stack.isEmpty()){//if  empty then no smaller to right
+                if(stack.isEmpty()){//if  empty then no smaller to left
                     li.add(-1);
                 }else{
-                    li.add(stack.peek());// top is the required right smaller
+                    li.add(stack.peek());// top is the required left greater
                 }
                 stack.push(arr[i]);
             }
-            Collections.reverse(li);
+            
             return li;
             
     }
