9a5242f4746c6cd7d8a21ceb19a94c16768be2f8
Debajyoti18 <bdebajyoti22@.com>
2025-09-30 20:48:04 +0530

    Generic tree basics construction

---
+++ GenericTrees/GenericTree.java
@@ -0,0 +1,53 @@
+package GenericTrees;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+class Node{
+    int val;
+    ArrayList<Node> childerns;
+    public Node(int x){
+        this.val = x;
+        this.childerns = new ArrayList<>();
+    }
+    public Node(){
+        this.val = 0;
+         this.childerns = new ArrayList<>();
+    }
+}
+
+public class GenericTree {
+    public static void main(String[] args) {
+         int[] arr = {10, 20, -1, 30, 50, -1, -1, 40, -1, -1};
+        
+        Node root = constructTree(arr);
+        
+        System.out.println("Tree Display:");
+        display(root);
+    }
+    private static void display(Node root) {
+        
+    }
+    public static Node constructTree(int[]  arr){
+            if( arr.length == 0 || arr == null){
+                return null;
+            }
+                Node root = null;
+                Stack<Node> stack = new Stack<>();
+                for(int i = 0; i < arr.length;i++){
+                    if( arr[i] != -1){
+                    Node temp = new Node(arr[i]);
+                    if(stack.isEmpty()){
+                        root = temp;
+                    }else{
+                        Node parent = stack.peek();
+                        parent.childerns.add(temp);
+                    }
+                    stack.push(temp);
+                }else{
+                    stack.pop();
+                }
+            }
+            return root;
+    }
+}
