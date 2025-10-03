a25f9d7c3a02bd32c7f13a2cfecfed292524c3fd
Debajyoti18 <bdebajyoti22@.com>
2025-10-03 18:31:13 +0530

    search target in a tree

--- GenericTrees/SearchInTree.java
+++ GenericTrees/SearchInTree.java
@@ -2,11 +2,11 @@ package GenericTrees;
 
 public class SearchInTree {
     public static boolean  search(Node root , int data){
-        if( root.val == data){
+        if( root.val == data){//first check for parents 
             return true;
         }
         for(Node child : root.children){
-            boolean src = search(child, data);
+            boolean src = search(child, data);//check each child of the parent  
             if(src){
                 return true;
             }
