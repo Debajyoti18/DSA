95fdd719a87bde2b253827199d0078699fb594cc
Debajyoti18 <bdebajyoti22@.com>
2025-10-05 23:07:39 +0530

    linerize  the tree

---
+++ GenericTrees/Linearize.java
@@ -0,0 +1,22 @@
+package GenericTrees;
+
+public class Linearize {
+    private static void linearize(Node node){
+        for(Node chNode : node.children){
+            linearize(chNode);
+        }
+        while (node.children.size() > 1) {
+            Node lastNode = node.children.remove(node.children.size()-1);
+            Node secondlastNode = node.children.get(node.children.size()-1);
+            Node secondlastNodeTail = getTail(secondlastNode);
+            secondlastNodeTail.children.add(lastNode); 
+        }
+    }
+
+    private static Node getTail(Node root) {
+       while (root.children.size() == 1) {
+            root = root.children.get(0);
+       }
+       return root;
+    }
+}
