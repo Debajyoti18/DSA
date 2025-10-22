3cd79022d6c1cb59ef1377311dc0036453c57f71
Debajyoti18 <bdebajyoti22@.com>
2025-10-22 22:17:18 +0530

    updating

--- 09_BinarySearchTree/MaxMin.java
+++ 09_BinarySearchTree/MaxMin.java
@@ -1,5 +1,7 @@
 package 09_BinarySearchTree;
 
+// Size  , Height and diameter are depends upon the structure of the tree so it will be same for BST as well as Binary Tree
+// But Max and Min are depends upon the data  of BST
 public class MaxMin {
     public static int maxInBst(Node root){
         //Maximum value is must present at the rightmost node of BST
