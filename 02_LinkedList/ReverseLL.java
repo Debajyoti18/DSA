753b33929f1ff801ccefd3f0f954e7ee25e84272
Debajyoti18 <bdebajyoti22@.com>
2025-11-24 21:12:56 +0530

    reversing LL with pointers...Updating

--- 02_LinkedList/ReverseLL.java
+++ 02_LinkedList/ReverseLL.java
@@ -1,11 +1,29 @@
 public class ReverseLL {
+
+    // Standard reverse
     public static Node reverseLL(Node head){
         Node prev = null, curr = head;
-        while( curr != null){
-            Node newnode =  curr.next;
+        while(curr != null){
+            Node next = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = newnode;
+            curr = next;
+        }
+        return prev;
+    }
+
+    // Three pointer version
+    public static Node reverseLLThreePointers(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node prev = null, curr = head, ford = head.next;
+
+        while (curr != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = ford;
+            if (ford != null) ford = ford.next;
         }
         return prev;
     }
