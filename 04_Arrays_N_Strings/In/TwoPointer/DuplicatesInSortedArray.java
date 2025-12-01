629728de0727e0fa469e98596dcda47051612a00
Debajyoti18 <bdebajyoti22@.com>
2025-12-01 11:11:04 +0530

    twosum2

---
+++ 04_Arrays_N_Strings/In/TwoPointer/DuplicatesInSortedArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    //Time Complexity: O(N) where N is the number of elements in the array
+    public int removeDuplicates(int[] nums) {
+        int lastUniqueIndex = 0;
+        for(int i = 1 ; i < nums.length ; i++){
+            if( nums[i] != nums[lastUniqueIndex]){
+            // unique element found so put it into next position
+                   lastUniqueIndex++;
+                   nums[lastUniqueIndex] = nums[i]; 
+            }
+        }
+        return lastUniqueIndex +1;
+    }
+}
