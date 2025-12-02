9a62419673a470ce2f7edd502e18499c464c319f
Debajyoti18 <bdebajyoti22@.com>
2025-12-02 21:11:10 +0530

    stack

---
+++ 04_Arrays_N_Strings/In/SlidingWindow/BestTimeToBuySellStock.java
@@ -0,0 +1,64 @@
+/**
+ * LeetCode Problem 121: Best Time to Buy and Sell Stock
+ * Intuition:
+ * - Use a single pass to track the minimum price seen so far.
+ * - Calculate potential profit at each price by subtracting the minimum price from the current price.
+ * - Update the maximum profit whenever a higher potential profit is found.
+ * Maintain minPrice = smallest price seen up to the current day. (This is the dynamic left of the window.)
+    For each current price p (the right of the window):
+    Potential profit = p - minPrice.
+    Update maxProfit = max(maxProfit, potential profit).
+    Update minPrice = min(minPrice, p) for future days.
+    This is one pass (O(n)), constant extra space (O(1)).
+ * Time Complexity: O(n)
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length < 2){ // no profit can be made if single day or empty
+            return 0;
+        }
+        int minPrice = Integer.MAX_VALUE,maxProfit = 0;
+        for( int p : prices){
+            if(p < minPrice){
+                minPrice = p;  // update minPrice if current price is lower
+            }
+           int profit = p - minPrice;
+            maxProfit = maxProfit > profit ? maxProfit : profit;// update maxProfit if current profit is higher
+
+        }
+        return maxProfit;
+    }
+}
+//**
+//  */ LeetCode Problem 122: Best Time to Buy and Sell Stock 2
+/**
+ * Intuition:
+ * - Iterate through the price array and sum up all positive differences between consecutive days.
+ * - This approach captures all upward trends, allowing for multiple buy-sell transactions.
+ * - Time Complexity: O(n)
+ * -Implementation:
+ * -Capture every increase in price from one day to the next as profit.
+ * -This effectively simulates buying at the start of each upward trend and selling at the end.
+ * -when dip then collect profit till then and start new transaction
+ */
+
+ class Solution {
+    public int maxProfit(int[] prices) {
+        int selldate = 0,buydate = 0,profit = 0;
+        for(int i = 1;i < prices.length;i++){
+            if(prices[i] >= prices[i-1]){
+                //upstroke so catch the max profit by moving up
+                selldate++;
+            }else{
+                //dip so collect max profit first
+                profit += prices[selldate] - prices[buydate];
+                buydate = selldate = i;
+            }
+        }
+        //for last tip
+        profit += prices[selldate] - prices[buydate];
+        return profit;
+
+
+    }
+}
--- 05_Stack_N_Queues/BalancedBrackets.java
+++ 05_Stack_N_Queues/BalancedBrackets.java
@@ -48,3 +48,25 @@ public class BalancedBrackets {
         }
     }
 }
+//Valid Parenthesis
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                 char c = stack.pop();
+                if((ch == ')' && c != '(') ||(ch == ']' && c != '[') ||(ch == '}' && c != '{')){
+                    return false;
+                }
+            
+            }
+        }
+        return stack.isEmpty();
+    }
+}
