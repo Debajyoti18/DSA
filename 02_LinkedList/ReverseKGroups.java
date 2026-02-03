d97638a324efc6b2b63440ba0e614942cb415724
Debajyoti18 <bdebajyoti22@.com>
2026-02-03 09:48:26 +0530

    updating

--- 02_LinkedList/ReverseKGroups.java
+++ 02_LinkedList/ReverseKGroups.java
@@ -44,3 +44,65 @@ class Solution {
             
     }
 }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    static ListNode th =null;
+    static ListNode tt = null;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || head.next == null || k==0) return head;
+
+        ListNode curr = head;
+        int len = getLength(head);
+        ListNode oh = null,ot=null;
+        while(len >= k){
+            int tempk = k;
+            while(tempk-- > 0){
+                ListNode ford = curr.next;
+                curr.next = null;
+                addFirst(curr);
+                curr = ford;
+            }
+            if(oh==null){
+                oh=th;
+                ot=tt;
+            }else{
+                ot.next = th;
+                ot=tt;
+            }
+            th =null;
+            tt=null;
+            len -= k;
+        }
+        if(ot != null){
+        ot.next = curr;
+        }
+        return oh;
+    }
+    void addFirst(ListNode head){
+        if(th == null){
+            th=head;
+            tt=head;
+        }else{
+            head.next = th;
+            th = head;
+        }
+    }
+    int getLength(ListNode head){
+         ListNode curr = head;
+         int count =0;
+         while(curr != null){
+                count++;
+                curr = curr.next;
+         }
+         return count;
+    }
+}
