b26e98d1f8724a6efaf0791d63e5795f08a3f3f7
Debajyoti18 <bdebajyoti22@gmail.com>
2026-02-18 20:14:50 +0530

    add MergeTwoSortedArray implementation

---
+++ 04_Arrays_N_Strings/Practice/TwoPointer/MergeTwoSortedArray.java
@@ -0,0 +1,18 @@
+public class MergeTwoSortedArray {
+    void mergeArrayByTwoPointer(int[] a,int m,int[] b,int n){
+        int i = m - 1;// At the last element  of the 1st array
+        int j = n - 1;//At the last index of 2nd Array
+        int k = m+n-1;//last free space o the 1st array
+        while(  i > 0 && j > 0){
+            // Greater element fills from the last of the 1st array
+            if( a[i] > b[j]){
+                a[k--] = a[i--];
+            }else{
+                a[k--] = b[j--];
+            }
+        }// add the extra elements of the 2nd array
+        while( j != 0){
+            a[k--] = b[j--];
+        }
+    }
+}
